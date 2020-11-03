4181356ca235b0555e3edd78320725654ace62ec
Nikita <mr.ububu@mail.ru>
2020-11-03 22:28:03 +0300

    add section 7,8

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // loop: true,
+      spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
